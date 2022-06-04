493059df47499abf7e044889ac0d3ceb9aa41536
applikers <dev.applikers@gmail.com>
2022-06-05 02:02:09 +0530

    ui fixes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,53 +1,64 @@
 $(document).ready(function () {
-    // popover
-    (function () {
-        $('[data-bs-toggle="popover"]').map(function (i, item) {
-            console.log(item)
-            return new bootstrap.Popover(item)
-        });
-    })();
+  if (location.host !== "localhost")
+    $("body")
+      .prev()
+      .append(`<base href="https://arunvbiradar.github.io/applikers-rahul/">`);
+  else $("body").prev().append(`<base href="http://localhost/ui/rahul/">`);
 
-    // navigation active link
-    (function () {
-        let baseUrl = document.baseURI;
-        let currentPage = location.href.slice(baseUrl.length);
-        $('[href="' + currentPage + '"]').addClass('active').parent().siblings().find('a').removeClass('active');
-    })();
+  // popover
+  (function () {
+    $('[data-bs-toggle="popover"]').map(function (i, item) {
+      console.log(item);
+      return new bootstrap.Popover(item);
+    });
+  })();
 
-    // testimonial
-    (function () {
-        $('.testimonial-slider').owlCarousel({
-            loop: true,
-            margin: 30,
-            dots: true,
-            smartSpeed: 700,
-            autoplay: 5000,
-            autoplayTimeout: 5000,
-            autoplayHoverPause: true,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                600: {
-                    items: 1
-                },
-                768: {
-                    items: 1
-                },
-                992: {
-                    items: 2
-                },
-                1200: {
-                    items: 2
-                }
-            }
-        })
-    })();
+  // navigation active link
+  (function () {
+    let baseUrl = document.baseURI;
+    let currentPage = location.href.slice(baseUrl.length);
+    $('[href="' + currentPage + '"]')
+      .addClass("active")
+      .parent()
+      .siblings()
+      .find("a")
+      .removeClass("active");
+  })();
 
-    // scroll top
-    (function () {
-        $('.scroll-top').on('click', function () {
-            $('html, body').animate({ scrollTop: 0 }, "slow");
-        });
-    })();
+  // testimonial
+  (function () {
+    $(".testimonial-slider").owlCarousel({
+      loop: true,
+      margin: 30,
+      dots: true,
+      smartSpeed: 700,
+      autoplay: 5000,
+      autoplayTimeout: 5000,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 1,
+        },
+        768: {
+          items: 1,
+        },
+        992: {
+          items: 2,
+        },
+        1200: {
+          items: 2,
+        },
+      },
+    });
+  })();
+
+  // scroll top
+  (function () {
+    $(".scroll-top").on("click", function () {
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+    });
+  })();
 });
